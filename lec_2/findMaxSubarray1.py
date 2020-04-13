fe80966c7c8d07d279415704f741041500dd0164
Kottoz <mohamed.t.elshetry@gmail.com>
2020-04-14 01:54:05 +0200

    lec_2

--- lec_2/findMaxSubarray1.py
+++ lec_2/findMaxSubarray1.py
@@ -1,11 +1,11 @@
-/**
+"""
  * @author Kotoz
  * @email mohamed.t.elshetry@mail.com
  * @create date 2020-04-14 00:38:59
  * @modify date 2020-04-14 00:38:59
- * @desc [Find Maximum subarray using bruteForce algorithim]
- */
-
+ * @desc [Find Maximum subarray using bruteForce algorithim, 
+ *        this algorithim is O(n^2)]
+"""
 
 
 def find_max_subArray(array):
@@ -17,7 +17,22 @@ def find_max_subArray(array):
             if sum > best_sum:
                 best_sum = sum
     return best_sum 
+    
+# def find_max_subArray(array):
+#     dic = {}
+#     sum = 0
+#     best_sum = float('-inf')
+#     for i in range(len(array)):
+#         sum = sum + array[i]
+#         dic[str(i)] = sum
+#         if sum > best_sum :
+#             best_sum = sum
+#     sum = 0 
+#     for index in range(len(array)):
+#         dic[str(index)]
+        
+
 
 arr = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-bs = find_max_subArray(arr)
-print(bs)
+d = find_max_subArray(arr)
+print(d)
--- lec_2/findMaxSubarray2.py
+++ lec_2/findMaxSubarray2.py
@@ -1,8 +1,8 @@
-/**
- * @author Kotoz
- * @email mohamed.t.elshetry@mail.com
- * @create date 2020-04-14 00:40:29
- * @modify date 2020-04-14 00:40:29
- * @desc [Find Maxsimuim subarray using divide and conquer approach]
- */
+
+#  * @author Kotoz
+#  * @email mohamed.t.elshetry@mail.com
+#  * @create date 2020-04-14 00:40:29
+#  * @modify date 2020-04-14 00:40:29
+#  * @desc [Find Maxsimuim subarray using divide and conquer approach]
  
+
