1adf8788e1c3b071ba62d136169931ab74331929
Kottoz <mohamed.t.elshetry@gmail.com>
2020-04-14 07:22:03 +0200

    find_max_subarray

--- lec_2/findMaxSubarray2.py
+++ lec_2/findMaxSubarray2.py
@@ -5,4 +5,22 @@
 #  * @modify date 2020-04-14 00:40:29
 #  * @desc [Find Maxsimuim subarray using divide and conquer approach]
  
-
+def find_maximum_cross(A, low, mid, high):
+    #left side
+    sum = 0 
+    left_sum = float('-inf')
+    for i in range(mid,low-1, -1):
+        sum += A[i]
+        if sum > left_sum:
+            left_sum = sum 
+            best_left = i
+    # return left_sum , best_left
+    #right side 
+    sum = 0 
+    right_sum = float('-inf')
+    for j in range(mid+1, len(A)):
+        sum += A[j]
+        if sum > right_sum:
+            right_sum = sum
+            best_right = j
+    return best_left, best_right, left_sum+right_sum
