5854d851e281415d6105d2eedba6a3f6ba5585b8
Kottoz <mohamed.t.elshetry@gmail.com>
2020-04-18 07:24:43 +0200

    Update insertion_sort.py

--- lec_3/insertion_sort.py
+++ lec_3/insertion_sort.py
@@ -5,15 +5,15 @@
 #  * @modify date 2020-04-16 12:19:17
 #  * @desc [description]
 #  */
-
+#O(n^2)
 def insertion_sort(A):
-    for j in range(len(A)):
-        key = A[j]
-        i = j - 1
-        while i >= 0 and A[i]>key:
-            A[i+1] = A[i]
-            i = i - 1
-        A[i+1] = key
+    for j in range(len(A)): #O(1) -- O(len(A))[O(n)]
+        key = A[j]          #O(1)
+        i = j - 1           #O(1)    
+        while i >= 0 and A[i]>key:#O(n)  
+            A[i+1] = A[i]           #O(1)
+            i = i - 1           #O(1)
+        A[i+1] = key            #O(1)
 
 arr = [5, 3, 2, 4 ,1]
 insertion_sort(arr)
