7f5d42d47cd6250a7c59e3be243ddc6fb2eebfee
Kottoz <mohamed.t.elshetry@gmail.com>
2020-04-20 06:40:10 +0200

    Update mergeSort.py

--- lec_3/mergeSort.py
+++ lec_3/mergeSort.py
@@ -1,22 +1,27 @@
-def mergeSort(A, f, l):
-    if f == l :
-        return A[0]
-    else:
-        m = (l - f) // 2
-        L  = mergeSort(A, f, m)
-        R  = mergeSort(A, m+1, l)
-        A = merge(A, L, R)
-        return A
+def mergeSort(A, p, r):
+    q = (p+r)//2
+    if p < r:
+        mergeSort(A, p, q)
+        mergeSort(A, q+1, r)
+        merge(A, p, q, r)
 
-def merge(A, L, R):
-    for i in range(len(A)):
-        if L[i]<R[i]:
-            A[i] = L[i]
+def merge(A, p, q, r):
+    L = [A[i] for i in range(p, q)]
+    R = [A[i] for i in range(q+1, r)]
+    L.append(float('inf'))
+    R.append(float('inf'))
+    i = 0 
+    j = 0  
+    for k in range(p, r):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
         else:
-            A[i] = R[i]
-        return A
+            A[k] = R[j]
+            j += 1 
+
     
 
 arr = [2, 4 ,1, 0]
-arr = mergeSort(arr, 0, len(arr))
+mergeSort(arr, 0, len(arr))
 print(arr)
