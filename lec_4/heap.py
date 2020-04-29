fc04d4788873332a7a6eaae191da4543a926cd39
Kottoz <mohamed.t.elshetry@gmail.com>
2020-04-29 10:50:35 +0200

    Update heap.py

--- lec_4/heap.py
+++ lec_4/heap.py
@@ -13,3 +13,24 @@ class heap(object):
         self.heap_list.append(k)
         self.heap_size = self.heap_size+1
         self.prec_up(self.heap_size)
+    def prec_down(self, i):
+        left_child  = i*2 +1 
+        right_child = i*2 +2
+        smallest_element = i 
+        while i < self.heap_size :
+            if left_child < self.heap_list[i]:
+                smallest_element = left_child
+            if right_child < smallest_element:
+                smallest_element = right_child
+            if i != smallest_element:
+                self.heap_list[i], self.heap_list[smallest_element] = self.heap_list[smallest_element], self.heap_list[i]
+            i = smallest_element
+    def del_min(self):
+        ret_val = self.heap_list[0]
+        #swap 
+        self.heap_list[0] = self.heap_list[self.heap_size]
+        self.heap_size = self.heap_size - 1
+        self.heap_list.pop()
+        self.prec_down(1)
+        return ret_val
+             
