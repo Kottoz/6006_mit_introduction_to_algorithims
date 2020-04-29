f370a1ce40bf461f598507c5355daa2e267acc18
Kottoz <mohamed.t.elshetry@gmail.com>
2020-04-29 10:36:27 +0200

    Update heap.py

--- lec_4/heap.py
+++ lec_4/heap.py
@@ -2,4 +2,14 @@ class heap(object):
     def __init__(self):
         self.heap_list = [0]
         self.heap_size = 0 
-    
+    def prec_up(self, i):
+        while i > 0 :
+            if self.heap_list[i] < self.heap_list[i//2]:
+                temp = self.heap_list[i//2]
+                self.heap_list[i//2] = self.heap_list[i]
+                self.heap_list[i] = temp 
+            i = i//2 
+    def insert(self, k):
+        self.heap_list.append(k)
+        self.heap_size = self.heap_size+1
+        self.prec_up(self.heap_size)
