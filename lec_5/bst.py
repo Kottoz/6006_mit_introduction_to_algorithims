2a421adbd74bcb915d4f122567d87323b4843cee
Kottoz <mohamed.t.elshetry@gmail.com>
2020-05-07 15:18:50 +0200

    Update bst.py

--- lec_5/bst.py
+++ lec_5/bst.py
@@ -1,4 +1,4 @@
-import treenode
+import treenode as tn
 class BinarySearchTree:
     def __init__(self):
         self.root = None
@@ -14,6 +14,24 @@ class BinarySearchTree:
         if self.root:
             self._put(key, val, self.root)
         else:
-            self.root = treenode.TreeNode(key, val)
+            self.root = tn.TreeNode(key, val)
+        self.size = self.size+1
+    
+    def _put(self, key, val, current_node):
+        if key < current_node.key:
+            if current_node.has_left_child():
+                self._put(key, val, current_node.left_child)
+            else: 
+                current_node.left_child = tn.TreeNode(key, val,
+                        parent=current_node)
+        else: 
+            if current_node.has_right_child():
+                self._put(key, val, current_node.right_child)
+            else: 
+                current_node.right_child = tn.TreeNode(key, val,
+                        parent= current_node)
+
+
+
 
 
