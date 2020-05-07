4474ddf2cee568c08aadf33c88f309f4099bdb99
Kottoz <mohamed.t.elshetry@gmail.com>
2020-05-07 15:05:49 +0200

    Update bst.py

--- lec_5/bst.py
+++ lec_5/bst.py
@@ -10,9 +10,10 @@ class BinarySearchTree:
     def __len__(self):
         return self.size
     
-    def put(slef, key, val):
+    def put(self, key, val):
         if self.root:
             self._put(key, val, self.root)
         else:
-            self.root = TreeNode
+            self.root = treenode.TreeNode(key, val)
+
 
